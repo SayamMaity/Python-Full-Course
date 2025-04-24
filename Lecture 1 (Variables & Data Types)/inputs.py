da543fe0b2110fc8c50def50015cca57f3fb14fa
Sayam Maity <sayammaity03@gmail.com>
2025-04-25 00:39:18 +0530

    Initial commit

---
+++ Lecture 1 (Variables & Data Types)/inputs.py
@@ -0,0 +1,20 @@
+# input("enter ur name ")
+name=input("enter ur name ")
+print("you entered",name)
+
+age=input("enter ur age ")
+print(type(age),age)
+
+age=int(input("enter ur age " )) # converting string to int
+print(type(age),age) # now age is int
+
+age=float(input("enter ur age " )) # converting int to float
+print(type(age),age) # now age is float
+
+
+urname=input("enter ur name ")
+urage=int(input("enter ur age " )) # converting string to int
+urmarks=float(input("enter ur marks " )) # converting string to float
+print("your name is",urname)
+print("your age is",urage)
+print("your marks are",urmarks)
---
+++ Lecture 1 (Variables & Data Types)/practice.py
@@ -0,0 +1,25 @@
+# write a program to take 2 numbers as input and find their sum
+
+first=int(input("enter first number "))
+second=int(input("enter second number "))
+sum=first + second # sum of first and second
+print("sum of",first,"and",second,"is",sum)
+
+
+#WAP to input side a square and print its area
+
+side=float(input("enter side of square "))
+area=side*side
+print("area of square is",area)
+
+#WAP to take 2 float values and print their average
+
+c=float(input("enter first number "))
+d=float(input("enter second number "))
+avg=(c+d)/2
+print("average of",c,"and",d,"is",avg)
+
+#WAP to take 2 integers as a and b and peint true if a is greater than or equal to b and false otherwise
+a=int(input("enter first number "))
+b=int(input("enter second number "))
+print(a>=b) # true if a is greater than or equal to b and false otherwise
