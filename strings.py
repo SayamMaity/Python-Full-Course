6576a25f10d7e390ea8b1686660115e0bb7c00cf
Sayam Maity <sayammaity03@gmail.com>
2025-06-07 03:05:07 +0530

    Initial commit

--- strings.py
+++ strings.py
@@ -32,3 +32,30 @@ print(len1) #5
 finalstr1= str9 + " beautiful" + str10 #concatenation
 print(finalstr1) #hello world
 print(len(finalstr1)) #length of string
+
+finalstr1= str9 + " " + str10 #concatenation
+print(finalstr1) #hello world
+print(len(finalstr1)) #length of string
+
+
+#indexing
+
+str="hello world"
+print(str[0]) #h
+print(str[5]) #space
+
+print(str[-1]) #d
+
+# str[2] = "d"# TypeError: 'str' object does not support item assignment
+# print(str)
+
+#slicing
+print(str[0:5]) #hello
+print(str[6:]) #world
+print(str[:4]) #hell
+print(str[0:6]) #hello 
+
+print(str[2:len(str)]) #llo world
+
+print(str[-5:-1]) #world
+print(str[-1:-4]) #empty string, slicing does not work in reverse order
