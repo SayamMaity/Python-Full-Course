9987356c18ae9cbb015c3d64a0be11ed842e6df8
Sayam Maity <sayammaity03@gmail.com>
2025-06-13 18:42:13 +0530

    Initial commit

--- strings.py
+++ strings.py
@@ -59,5 +59,4 @@ print(str[2:len(str)]) #llo world
 
 print(str[-5:-1]) #world
 print(str[-1:-4]) #empty string, slicing does not work in reverse order
-print(str[-5:])
-print(str[-5:-1]) #worl
+print(str[-5:]) #world
