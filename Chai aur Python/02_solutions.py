98cbee5f3908c62f268b27ce8184a76c8d61ec56
Sayam Maity <sayammaity03@gmail.com>
2025-12-12 03:31:27 +0530

    Initial commit

---
+++ Chai aur Python/02_solutions.py
@@ -0,0 +1,9 @@
+#Movie Ticket Pricing
+
+age= int(input("Pls tell your age:"))
+
+if age >18:
+    print("You are an adult have to pay $12 for the movie ticket.")
+else:
+    print("You are a child have to pay $8 for the movie ticket.")
+
