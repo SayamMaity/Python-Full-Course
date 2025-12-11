4a729e7a42ceaa0fd92c806e7233a87290c83ce5
Sayam Maity <sayammaity03@gmail.com>
2025-12-12 03:29:21 +0530

    Initial commit

---
+++ Chai aur Python/01_basics/Conditionals/01_solutions.py
@@ -0,0 +1,24 @@
+age= int(input("Provide me your age: "))
+if (age < 13):
+    print("You are a child.")
+elif (13<= age <=19):
+    print("You are a teenager.")
+elif (20<= age <=59):
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+
+
+
+'''Sir's solution
+
+age = 65
+
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior")'''
